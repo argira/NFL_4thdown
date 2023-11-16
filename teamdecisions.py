2b070fe3772bb47a590c358adb6265b38f176d33
argira <argira@hotmail.com>
2023-11-15 20:30:20 -0800

    Update teamdecisions.py

--- teamdecisions.py
+++ teamdecisions.py
@@ -41,7 +41,7 @@ def app():
         data = df[df["home_team"]==team]
         data = data[data["season"]==season]
         keep_columns = ['game_date','play_type','ydstogo','away_team','game_seconds_remaining']
-        scoreboard_columns = ['play_type','ydstogo','Decision','game_seconds_remaining','game_half','yardline_100','qtr','side_of_field','posteam_score','defteam_score']
+        scoreboard_columns = ['away_team','play_type','ydstogo','Decision','game_seconds_remaining','game_half','yardline_100','qtr','side_of_field','posteam_score','defteam_score']
         display_df = data[keep_columns]
         display_df = display_df.rename(columns={"game_date": "Date", "play_type": "Play Type", "ydstogo": "Yards to Go"}, errors="raise")
         
@@ -71,8 +71,8 @@ def app():
          plot_df = plot_df[plot_df['Decision']==decision]
 
         with col2:
-          
-          st.image('/image'+data['defteam']+'.png') 
+
+          st.image('/image'+data['away_team']+'.png') 
           st.markdown("Show the Scoreboard")
 
           def create_football_field(linenumbers=True,
