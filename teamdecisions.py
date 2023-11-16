3fe98a4d2e374f0f4acaca0f3199f49a5e62e934
argira <argira@hotmail.com>
2023-11-16 13:58:32 -0800

    Update teamdecisions.py

--- teamdecisions.py
+++ teamdecisions.py
@@ -20,6 +20,8 @@ def app():
   def change_label_style(label, font_size='12px', font_color='black', font_family='sans-serif'):
     html = f"""
     <script>
+        var elems = window.parent.document.querySelectorAll('p');
+        var elem = Array.from(elems).find(x => x.innerText == '{label}');
         var elem = '{label}';
         elem.style.fontSize = '{font_size}';
         elem.style.color = '{font_color}';
