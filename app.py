4440877a7141d86a3c5573ac83fae6fe4a9f2273
argira <argira@hotmail.com>
2023-11-27 09:41:47 -0800

    trying d3

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import teamdecisions
-#import bestdecisions
-import worstdecisions
-import topteams
+import bestdecisions
+#import worstdecisions
+#import topteams
 import streamlit as st
 
 
 
 OPTIONS = { "Team Decisions": teamdecisions,
-           #"Best Decisions": bestdecisions,
-           "Worst Decisions": worstdecisions,
-           "4th Top Performer Teams": topteams
+           "Best Decisions": bestdecisions,
+           #"Worst Decisions": worstdecisions,
+           #"4th Top Performer Teams": topteams
           }
 
 st.sidebar.title('Navigation')
