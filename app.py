586c82d2c0e9083ce3c96eed4b12d18d8d918874
argira <argira@hotmail.com>
2023-11-27 09:45:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import teamdecisions
-import bestdecisions
+#import bestdecisions
 #import worstdecisions
 #import topteams
 import streamlit as st
 
 
 
-OPTIONS = { "Team Decisions": teamdecisions,
-           "Best Decisions": bestdecisions,
+OPTIONS = { "Team Decisions": teamdecisions
+           #"Best Decisions": bestdecisions,
            #"Worst Decisions": worstdecisions,
            #"4th Top Performer Teams": topteams
           }
