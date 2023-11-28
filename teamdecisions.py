93e2d7c71fc706f50a66c24b2fdf5cd9e6eb1e5e
argira <argira@hotmail.com>
2023-11-27 18:10:29 -0800

    Update teamdecisions.py

--- teamdecisions.py
+++ teamdecisions.py
@@ -166,9 +166,10 @@ def app():
 
     graph_data = game_df[cols_graphic]
     graph_data['minutes_remaining'] = (graph_data['game_seconds_remaining']/60).astype(int)
+    
+    graph_data = graph_data[graph_data['minutes_remaining']<=decision_time]
     graph_data = graph_data.set_index("minutes_remaining").sort_index(ascending=False).rename(columns={"home_wp":game_teams[0],
                                 "away_wp":game_teams[1]})
-    graph_data = graph_data[graph_data['minutes_remaining']<=decision_time]
 
 
     tab1, tab2 = st.tabs(["Field Position", "Win Probability chart"])
