4fbe8c1f35a400b99fd83a08ae0aef306d43875e
argira <argira@hotmail.com>
2023-11-29 18:27:42 -0800

    Update teamdecisions.py

--- teamdecisions.py
+++ teamdecisions.py
@@ -181,7 +181,7 @@ def app():
     
 
     with tab1:
-      st.header("Field Position of team "+team1[0])
+      st.write("Field Position of team "+team1[0])
       plt.figure()
 
       yl=100-yard_line.astype(int)
