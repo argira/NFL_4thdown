619c1e5aa3070e4f02a2fabcab86631dfbd7eb2b
argira <argira@hotmail.com>
2023-11-29 16:13:41 -0800

    Update teamdecisions_2.py

--- teamdecisions_2.py
+++ teamdecisions_2.py
@@ -232,14 +232,12 @@ def app():
       play_columns = ['posteam_fg_made_wp_delta', 'posteam_fg_missed_wp_delta', 'posteam_punt_wp_delta','posteam_pass_failed_wp_delta', 'posteam_run_failed_wp_delta', 'posteam_pass_convert_wp_delta', 'posteam_run_convert_wp_delta']
       column_graph = plot_df[plot_df['play_id']==decision_play[0]]
       column_graph = column_graph[play_columns]
-      graph_df = []
-      graph_df['Punt']=column_graph['posteam_punt_wp_delta']
-      graph_df['Field Goal']=column_graph['posteam_fg_made_wp_delta']+column_graph['posteam_fg_missed_wp_delta']
-      graph_df['Run']=column_graph['posteam_run_failed_wp_delta'] + column_graph['posteam_run_convert_wp_delta']
-      graph_df['Pass']=column_graph['posteam_pass_failed_wp_delta'] + column_graph['posteam_pass_convert_wp_delta']
-      #column_graph = column_graph.rename(columns={"posteam_fg_made_wp_delta":'FG Made',
-       #                         "posteam_fg_missed_wp_delta":'FG Missed', 'posteam_punt_wp_delta':'Punt'})
-      graph_df = graph_df.T
+      column_graph['Punt']=column_graph['posteam_punt_wp_delta']
+      column_graph['Field Goal']=column_graph['posteam_fg_made_wp_delta']+column_graph['posteam_fg_missed_wp_delta']
+      column_graph['Run']=column_graph['posteam_run_failed_wp_delta'] + column_graph['posteam_run_convert_wp_delta']
+      column_graph['Pass']=column_graph['posteam_pass_failed_wp_delta'] + column_graph['posteam_pass_convert_wp_delta']
+      column_graph = column_graph.drop(columns=['posteam_fg_made_wp_delta', 'posteam_fg_missed_wp_delta', 'posteam_punt_wp_delta','posteam_pass_failed_wp_delta', 'posteam_run_failed_wp_delta', 'posteam_pass_convert_wp_delta', 'posteam_run_convert_wp_delta'])
+      graph_df = column_graph.T
 
       
       st.markdown("Change in Win Probability by play decision")
